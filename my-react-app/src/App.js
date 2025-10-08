a494bf552830d85abf9ac8ba4341c4c20c680725
chipkam <khoachipnau@gmail.com>
2025-10-08 17:23:05 +0700

    Scroll bar + checked (contents in Dashboard)

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,37 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 import { HRNavbar } from './components/Navbar';
+import CustomScrollbar from './components/schollbar';
 import Dashboard from './pages/dashboard';
 import {
     BrowserRouter as Router,
     Routes,
     Route,
 } from "react-router-dom";
+
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="h-screen flex flex-col">
         <HRNavbar />
         
-        <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/employeemanagement" element={<h1>Employee Management Page</h1>} />
-          <Route path="/contract" element={<h1>Contract Page</h1>} />
-          <Route path="/timetracking" element={<h1>Time Tracking Page</h1>} />
-          <Route path="/" element={
-       
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1 style={{ fontFamily: 'Baloo 2, sans-serif', color: '#06007c' }}>
-                  Welcome to HR Management System
-                </h1>
-                <p style={{ fontFamily: 'Baloo 2, sans-serif', color: '#666' }}>
-                  Click on Dashboard in the navigation to get started!
-                </p>
-              </header>
-           
-          } />
-        </Routes>
+        <main className="flex-1 relative">
+
+          <CustomScrollbar>
+            {/* <div className="p-4 md:p-8"> dùng để canh lề (margin)*/}
+            <div className="p-4 md:p-8">
+              <Routes>
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/employeemanagement" element={<h1>Employee Management Page</h1>} />
+                <Route path="/contract" element={<h1>Contract Page</h1>} />
+                <Route path="/timetracking" element={<h1>Time Tracking Page</h1>} />
+                <Route path="/" element={
+                  <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1>Welcome to HR Management System</h1>
+                    <p>Click on Dashboard in the navigation to get started!</p>
+                  </header>
+                } />
+              </Routes>
+            </div>
+          </CustomScrollbar>
+        </main>
       </div>
     </Router>
   );

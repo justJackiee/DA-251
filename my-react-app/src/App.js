90d58622f1ff70181d4b6899a4e157f8abdec491
unknown <khang.duong2311403@hcmut.edu.vn>
2025-10-08 11:52:46 +0700

    fix

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import { HRNavbar } from './components/Navbar';
+import Pagination from './components/pagination'
 import Dashboard from './pages/dashboard';
 import {
     BrowserRouter as Router,
     Routes,
     Route,
 } from "react-router-dom";
+import { ThemeProvider } from "@material-tailwind/react";
 function App() {
   return (
+    <ThemeProvider>
     <Router>
       <div className="App">
         <HRNavbar />
@@ -18,6 +21,7 @@ function App() {
           <Route path="/employeemanagement" element={<h1>Employee Management Page</h1>} />
           <Route path="/contract" element={<h1>Contract Page</h1>} />
           <Route path="/timetracking" element={<h1>Time Tracking Page</h1>} />
+          <Route path="/pagination" element={<Pagination />}/>
           <Route path="/" element={
        
               <header className="App-header">
@@ -34,6 +38,7 @@ function App() {
         </Routes>
       </div>
     </Router>
+    </ThemeProvider>
   );
 }
 
